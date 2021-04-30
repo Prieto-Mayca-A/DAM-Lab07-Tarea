f5c5b32e578e5e648b0c09d2b3e170f50dc2fdad
Prieto-Mayca-A <68166806+Prieto-Mayca-A@users.noreply.github.com>
2021-04-30 09:38:04 -0500

    original commit

--- src/helpers/emailValidator.js
+++
@@ -1,6 +0,0 @@
-export function emailValidator(numero1) {
-  const re = /\S+@\S+\.\S+/
-  if (!numero1) return "Email no valido."
-  if (!re.test(numero1)) return 'Incorrecto! Necesitamos una dirección de correo electrónico válida.'
-  return ''
-}
